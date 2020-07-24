2742115999bd12e855f7b603321f329b53954ebc
ShobhanKarthick <63546843+ShobhanKarthick@users.noreply.github.com>
2020-07-24 15:27:45 +0530

    login and register UI change

--- client/src/Components/UserAccount/Register.js
+++ client/src/Components/UserAccount/Register.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Menubar from './Menubar'
 import axios from 'axios'
 
@@ -9,6 +9,35 @@ function Register() {
     const [password, setPassword] = useState('')
     const [confirmPassword, setConifrmPassword] = useState('')
     const [phone, setPhone] = useState('')
+
+    useEffect(() => {
+        document.getElementById("name").style.display = "none"
+        document.getElementById("password").style.display = "none"
+        document.getElementById("confirmPassword").style.display = "none"
+        document.getElementById("phone").style.display = "none"
+        document.getElementById("register-button").style.display = "none"
+        return
+    }, [])
+
+    const emailCheck = () => {
+        axios.post("/users/checkuser", {username: email})
+        .then(response => {
+            if(response.data.error === "Found"){
+                alert("You have already registered")
+            }
+            if(response.data.error === "Not Found"){
+                document.getElementById("name").style.display = "block"
+                document.getElementById("password").style.display = "block"
+                document.getElementById("confirmPassword").style.display = "block"
+                document.getElementById("phone").style.display = "block"
+                document.getElementById("next-button").style.display = "none"
+                document.getElementById("register-button").style.display = "block"
+            }
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
 
     const nameHandler = (event) => {
         setName(event.target.value)
@@ -62,12 +91,13 @@ function Register() {
         <Menubar type="register" />
         <p className="register-login-head">Register</p>
         <form id="register-form" className="register-login-form" onSubmit={submitHandler}>
-        <input className="register-login-input" type="text" value={name} onChange={nameHandler} placeholder="Name" required/>
-        <input className="register-login-input" type="email" value={email} onChange={emailHandler} placeholder="E-mail" required/>
-        <input className="register-login-input" type="password" value={password} onChange={passwordHandler} pattern=".{8,}" title="Your password must 8 or more characters" placeholder="Password" required/>
-        <input className="register-login-input" type="password" value={confirmPassword} onChange={confirmPasswordHandler} pattern={password} title="Password do not match" placeholder="Confirm Password" required/>
-        <input className="register-login-input" type="text" value={phone} onChange={phoneHandler} pattern="[0-9]{10}" maxLength="10" placeholder="Phone" required/>
-        <button className="register-login-button"  form="register-form" type="submit">SIGN UP</button>
+        <input id="email" className="register-login-input" type="email" value={email} onChange={emailHandler} placeholder="E-mail" required/>
+        <input id="name" className="register-login-input" type="text" value={name} onChange={nameHandler} placeholder="Name" required/>
+        <input id="password" className="register-login-input" type="password" value={password} onChange={passwordHandler} pattern=".{8,}" title="Your password must 8 or more characters" placeholder="Password" required/>
+        <input id="confirmPassword" className="register-login-input" type="password" value={confirmPassword} onChange={confirmPasswordHandler} pattern={password} title="Password do not match" placeholder="Confirm Password" required/>
+        <input id="phone" className="register-login-input" type="text" value={phone} onChange={phoneHandler} pattern="[0-9]{10}" maxLength="10" placeholder="Phone" required/>
+        <button id="next-button" className="register-login-button" onClick={emailCheck}>NEXT</button>
+        <button id="register-button" className="register-login-button"  form="register-form" type="submit">SIGN UP</button>
         </form>
         </div>
         </div>
