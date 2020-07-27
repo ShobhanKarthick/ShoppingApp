4eb99c2f5d573ad570345925596be9091cfbc2eb
ShobhanKarthick <63546843+ShobhanKarthick@users.noreply.github.com>
2020-07-27 14:12:36 +0530

    styling table

--- client/src/Components/Home/QuantityInput.js
+++ client/src/Components/Home/QuantityInput.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios';
 
 function QuantityInput(props) {
@@ -8,6 +8,19 @@ function QuantityInput(props) {
     const quantityHandler = (event) => {
         setQuantity(event.target.value);
     }
+
+    useEffect(() => {
+        if(quantity > 1){
+            document.getElementById("add-item").style.display = "none"
+            document.getElementById("increase-item").style.display = "block"
+            document.getElementById("decrease-item").style.display = "block"
+        }
+        else{
+            document.getElementById("add-item").style.display = "block"
+            document.getElementById("increase-item").style.display = "none"
+            document.getElementById("decrease-item").style.display = "none"
+        }
+    }, [quantity])
 
     const addProduct = () => {
 
@@ -25,12 +38,21 @@ function QuantityInput(props) {
 
     }
 
+    const decreaseItem = () => {
+        setQuantity(quantity - 1)
+    }
+
+    const increaseItem = () => {
+        setQuantity(quantity + 1)
+    }
 
     return (
         <div className="product-bottom-row">
             <label>Qty</label>
+            <button onClick={decreaseItem} id="decrease-item">-</button>
             <input id={props.product._id} type="text" pattern="[0-9]{2}" maxLength="2" title="Maximum quantiy of 99" placeholder="Qty" value={quantity} onChange={quantityHandler}/>
-            <button onClick={addProduct}>ADD</button>
+            <button onClick={increaseItem} id="increase-item">+</button>
+            <button id="add-item" onClick={addProduct}>ADD</button>
         </div>
     )
 }
